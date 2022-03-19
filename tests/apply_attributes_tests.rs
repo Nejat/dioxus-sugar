3c17248ee6e7abdb9459aea9b5ae65cdaf745ada
Nejat <nejatoz@gmail.com>
2022-03-19 08:44:48 -0400

    [update] updated attributes test

--- tests/apply_attributes_tests.rs
+++ tests/apply_attributes_tests.rs
@@ -1,19 +1,52 @@
+use dioxus::events::MouseEvent;
 use dioxus::prelude::*;
 
 use dioxus_sugar::attributes;
 
 #[attributes(optional(div), exclude(class, name, id))]
 #[derive(Props)]
-struct Props<'a> {
-    #[props(default)]
-    class: &'a str,
+pub struct ComponentProps<'a> {
+    #[props(default, inject_as = "class", attribute_on = "div:[1]; div:[0] > a")]
+    my_class: &'a str,
+    #[props(default, handler_on = "div")]
+    onclick: EventHandler<'a, MouseEvent>,
     children: Element<'a>,
 }
 
-fn component<'a>(cx: Scope<'a, Props<'a>>) -> Element {
-    cx.render(rsx! {
+#[allow(non_snake_case)]
+pub fn Component<'a>(cx: Scope<'a, ComponentProps<'a>>) -> Element {
+    rsx! {
+        cx: ComponentProps;
         div {
+            a {}
             &cx.props.children
         }
-    })
+        div {}
+    }
+}
+
+#[attributes(optional(div), exclude(class, name, id))]
+#[derive(Props)]
+pub struct Component2Prop<'a> {
+    // #[props(default)]
+    #[props(default, inject_as = "my_class", attribute_on = "div > Component")]
+    class: &'a str,
+    // #[props(default)]
+    #[props(default, handler_on = "div > Component")]
+    onclick: EventHandler<'a, MouseEvent>,
+    children: Element<'a>,
+}
+
+#[allow(non_snake_case)]
+pub fn Component2<'a>(cx: Scope<'a, Component2Prop<'a>>) -> Element {
+    rsx! {
+        cx: Component2Prop;
+        div {
+            Component {
+                &cx.props.children,
+            }
+            div {}
+        }
+        div {}
+    }
 }
